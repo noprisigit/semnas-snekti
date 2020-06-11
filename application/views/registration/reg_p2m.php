ce82c7b1b5d4fb90d294689d0c8793ffecd58b12
Sigit Prasetyo Noprianto <noprisigit@gmail.com>
2020-06-12 01:23:09 +0700

    update title file req_p2m.php

--- application/views/registration/reg_p2m.php
+++ application/views/registration/reg_p2m.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12 text-center">
-                <h2>Registrasi Pemakalah P2M</h2>
+                <h2>Registrasi P2M</h2>
             </div>
         </div>
     </div>
